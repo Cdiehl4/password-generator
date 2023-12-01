beeee4ccee4a2cd75ccc2ada9eb77b5506e6f1e5
Cdieh4 <you@example.com>
2023-12-01 15:54:59 -0600

    new change

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ function generatePassword(){
 //  Step 1:Gather user input
 // Step 2:Generate random string
 // Step 3:Return string
-return "12345"
+return "069150"
 } 	
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
